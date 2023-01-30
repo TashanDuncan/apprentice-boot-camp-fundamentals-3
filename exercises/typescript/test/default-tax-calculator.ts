19a1d482944eaafe41052d4cadb647feef33f560
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-01-30 15:16:45 +0000

    implement story 2

--- exercises/typescript/test/default-tax-calculator.ts
+++ exercises/typescript/test/default-tax-calculator.ts
@@ -1,11 +1,6 @@
 import { TaxCalculator } from "../tax-calculator";
 import { Vehicle } from "../vehicle";
 
-// const story1 = new Map([
-//   [0, 0],
-
-// ])
-
 class DefaultTaxCalculator extends TaxCalculator {
   constructor() {
     super(2023);
@@ -16,49 +11,46 @@ class DefaultTaxCalculator extends TaxCalculator {
       return 0;
     }
     if (vehicle.co2Emissions < 51) {
-      return 10;
-    }
-
-    if (vehicle.co2Emissions < 76) {
       return 25;
     }
 
-    if (vehicle.co2Emissions < 91) {
+    if (vehicle.co2Emissions < 76) {
       return 105;
     }
 
-    if (vehicle.co2Emissions < 101) {
+    if (vehicle.co2Emissions < 91) {
       return 125;
     }
 
-    if (vehicle.co2Emissions < 111) {
+    if (vehicle.co2Emissions < 101) {
       return 145;
     }
 
-    if (vehicle.co2Emissions < 131) {
+    if (vehicle.co2Emissions < 111) {
       return 165;
     }
 
-    if (vehicle.co2Emissions < 151) {
+    if (vehicle.co2Emissions < 131) {
       return 205;
     }
 
-    if (vehicle.co2Emissions < 171) {
+    if (vehicle.co2Emissions < 151) {
       return 515;
     }
 
-    if (vehicle.co2Emissions < 191) {
+    if (vehicle.co2Emissions < 171) {
       return 830;
     }
 
-    if (vehicle.co2Emissions < 226) {
+    if (vehicle.co2Emissions < 191) {
       return 1240;
     }
 
-    if (vehicle.co2Emissions <= 255) {
+    if (vehicle.co2Emissions < 226) {
       return 1760;
     }
-    if (vehicle.co2Emissions > 255) {
+
+    if (vehicle.co2Emissions <= 255 || vehicle.co2Emissions > 255) {
       return 2070;
     }
   }
