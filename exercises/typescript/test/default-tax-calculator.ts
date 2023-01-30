4aa888f5b05aa0c2e9821a08b2e08445b2befbf0
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-01-30 15:33:56 +0000

    add diesel calculations

--- exercises/typescript/test/default-tax-calculator.ts
+++ exercises/typescript/test/default-tax-calculator.ts
@@ -7,52 +7,57 @@ class DefaultTaxCalculator extends TaxCalculator {
   }
 
   calculateTax(vehicle: Vehicle) {
-    if (vehicle.co2Emissions === 0) {
-      return 0;
-    }
-    if (vehicle.co2Emissions < 51) {
-      return 25;
+    if(vehicle.fuelType === 'Diesel') {
+      if (vehicle.co2Emissions === 0) {
+
+        return 0;
+      }
+      if (vehicle.co2Emissions < 51) {
+        
+        return 25;
+      }
+  
+      if (vehicle.co2Emissions < 76) {
+        return 105;
+      }
+  
+      if (vehicle.co2Emissions < 91) {
+        return 125;
+      }
+  
+      if (vehicle.co2Emissions < 101) {
+        return 145;
+      }
+  
+      if (vehicle.co2Emissions < 111) {
+        return 165;
+      }
+  
+      if (vehicle.co2Emissions < 131) {
+        return 205;
+      }
+  
+      if (vehicle.co2Emissions < 151) {
+        return 515;
+      }
+  
+      if (vehicle.co2Emissions < 171) {
+        return 830;
+      }
+  
+      if (vehicle.co2Emissions < 191) {
+        return 1240;
+      }
+  
+      if (vehicle.co2Emissions < 226) {
+        return 1760;
+      }
+  
+      if (vehicle.co2Emissions <= 255 || vehicle.co2Emissions > 255) {
+        return 2070;
+      }
     }
 
-    if (vehicle.co2Emissions < 76) {
-      return 105;
-    }
-
-    if (vehicle.co2Emissions < 91) {
-      return 125;
-    }
-
-    if (vehicle.co2Emissions < 101) {
-      return 145;
-    }
-
-    if (vehicle.co2Emissions < 111) {
-      return 165;
-    }
-
-    if (vehicle.co2Emissions < 131) {
-      return 205;
-    }
-
-    if (vehicle.co2Emissions < 151) {
-      return 515;
-    }
-
-    if (vehicle.co2Emissions < 171) {
-      return 830;
-    }
-
-    if (vehicle.co2Emissions < 191) {
-      return 1240;
-    }
-
-    if (vehicle.co2Emissions < 226) {
-      return 1760;
-    }
-
-    if (vehicle.co2Emissions <= 255 || vehicle.co2Emissions > 255) {
-      return 2070;
-    }
   }
 }
 
--- exercises/typescript/test/tax-calculator-petrol.test.ts
+++ exercises/typescript/test/tax-calculator-petrol.test.ts
@@ -3,7 +3,7 @@ import { DefaultTaxCalculator } from './default-tax-calculator';
 import { Vehicle } from '../vehicle';
 import { FuelType } from '../fuel-type';
 
-describe.skip('Tax calculator on a petrol vehicle for the first year', () => {
+describe('Tax calculator on a petrol vehicle for the first year', () => {
   let taxCalculator = new DefaultTaxCalculator();
   let FIRST_OF_JAN_2020 = new Date(2020, 1, 1);
 
