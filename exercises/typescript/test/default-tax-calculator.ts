17a2329147ce4abcc9ed9be39b9eb1de2a989f6f
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-01-30 11:40:26 +0000

    implement tax calculation method

--- exercises/typescript/test/default-tax-calculator.ts
+++ exercises/typescript/test/default-tax-calculator.ts
@@ -1,7 +1,10 @@
 import { TaxCalculator } from '../tax-calculator';
 import { Vehicle } from '../vehicle';
 
+// const story1 = new Map([
+//   [0, 0],
 
+// ])
 
 class DefaultTaxCalculator extends TaxCalculator {
   constructor() {
@@ -9,7 +12,56 @@ class DefaultTaxCalculator extends TaxCalculator {
   }
 
   calculateTax(vehicle: Vehicle) {
-    return 0;
+    if(vehicle.co2Emissions === 0) {
+      return 0
+    }
+    if(vehicle.co2Emissions < 51) {
+      return 10
+    }
+
+    if(vehicle.co2Emissions < 76) {
+      return 25
+    }
+
+    if(vehicle.co2Emissions < 91) {
+      return 105
+    }
+
+    if(vehicle.co2Emissions < 101) {
+      return 125
+    }
+
+    if(vehicle.co2Emissions < 111) {
+      return 145
+    }
+
+    if(vehicle.co2Emissions < 131) {
+      return 165
+    }
+
+    if(vehicle.co2Emissions < 151) {
+      return 205
+    }
+
+    if(vehicle.co2Emissions < 171) {
+      return 515
+    }
+
+    if(vehicle.co2Emissions < 191) {
+      return 830
+    }
+
+    if(vehicle.co2Emissions < 226) {
+      return 1240
+    }
+
+
+    if(vehicle.co2Emissions <= 255) {
+      return 1760
+    }
+    else {
+      return 2070
+    }
   }
 }
 
